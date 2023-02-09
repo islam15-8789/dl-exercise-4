b9da1e19dcad9521d890d01c68a97d2bf5a8b179
Arni Islam <yb65eryq@stud.informatik.uni-erlangen.de>
2023-02-09 14:54:48 +0100

    updated

--- src_to_implement/data.py
+++ src_to_implement/data.py
@@ -11,5 +11,35 @@ train_std = [0.16043035, 0.16043035, 0.16043035]
 
 
 class ChallengeDataset(Dataset):
-    def __init__():
-        pass    
+    def __init__(self, data, mode, transform):
+        self.data = data
+        self.mode = mode
+        self._transform = transform    
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
+
+        image_path = str(Path("ex_4/src_to_implement/", self.data.iloc[index, 0]))
+        image_label = torch.as_tensor(
+            [self.data.iloc[index, 1], self.data.iloc[index, 2]]
+        )
+        image = imread(image_path)
+        image = gray2rgb(image=image)
+        transform = tv.transforms.Compose(
+            [
+                tv.transforms.ToTensor(),
+                tv.transforms.Normalize(
+                    mean=torch.tensor(train_mean),
+                    std=torch.tensor(train_std),
+                    inplace=True,
+                ),
+            ]
+        )
+        image = transform(image)
+        if self._transform:
+            image = self._transform(image)
+        return image.to(torch.float32), image_label.to(torch.float32)
